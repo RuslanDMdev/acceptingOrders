ec412a0cf83fcfaa54037ed05f76cad938ab5d90
RuslanDMdev <deelcrafting@gmail.com>
2021-11-25 14:52:41 +0300

    commit

--- accepORders/accepORders/CheksViewController.swift
+++ accepORders/accepORders/CheksViewController.swift
@@ -10,22 +10,36 @@ import UIKit
 class CheksViewController: UIViewController {
     
     var orders = [ Orders(FIO: " ", tableNum: " ", numOfGuests: " ") ]
+    
+    private var data = ["Ios", "Android", "Hello", "Google", "hi", "Stive", "Yhooy", "Bing", "Yandex", "Sber"]
    
-
+    let tableView = UITableView.init(frame: .zero, style: UITableView.Style.grouped)
    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        super.viewDidLoad()
-        
-        
-      
+        self.view.backgroundColor = UIColor.white
+        self.navigationItem.title = "Чеки"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Назад", style: .plain, target: self, action: #selector(somefunc))
         
-        self.navigationItem.title = "Чеки"
+        self.view.addSubview(self.tableView)
+        self.tableView.register(OrderTableViewCell.self, forCellReuseIdentifier: "OrderTableViewCell")
+        self.tableView.dataSource = self
+        
+        self.updateLayout(with: self.view.frame.size)
         
     }
-
+    
+    private func updateLayout(with size: CGSize) {
+        self.tableView.frame = CGRect.init(origin: .zero, size: size)
+     }
+    
+     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: { (contex) in
+           self.updateLayout(with: size)
+        }, completion: nil)
+     }
     
     @objc func somefunc(){
         let rootVC = OrderViewController()
@@ -36,3 +50,18 @@ class CheksViewController: UIViewController {
 
     
 }
+
+extension CheksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
+        cell.textLabel?.text = self.data[indexPath.row]
+        return cell
+    }
+    
+}
+
